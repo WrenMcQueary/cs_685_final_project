ea178c72e6604d2bebf15b95b5d093c56be98d44
Wren McQueary <wren.mcqueary@gmail.com>
2022-11-02 20:55:20 -0400

    Removed old workspace folder

--- workspace_folder/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wren/Documents/CS685/jupyter_notebooks/homework_assignments/P1_ROS_kinematics_control/workspace_folder/src/beginner_tutorials
-# Build directory: /home/wren/Documents/CS685/jupyter_notebooks/homework_assignments/P1_ROS_kinematics_control/workspace_folder/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- workspace_folder/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/wren/Documents/CS685/jupyter_notebooks/homework_assignments/P1_ROS_kinematics_control/workspace_folder/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
