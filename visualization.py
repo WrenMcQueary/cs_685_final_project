fd9d85b69b51ea29a53bf1a307893c8277699ffe
Wren McQueary <wren.mcqueary@gmail.com>
2022-11-13 16:41:28 -0500

    First correct draft of arm visualization code!

--- visualization.py
+++ visualization.py
@@ -27,13 +27,16 @@ def draw_snapshot(l_0: float, l_1: float, theta_0: float, theta_1: float, theta_
                          [0],
                          [0],
                          [1]])
-    transform_base_to_joint_1 = get_translation_x_transform(l_0) @ get_rotation_y_transform(-theta_1) @ get_rotation_z_transform(theta_0)
+    transform_base_to_joint_1 = get_rotation_z_transform(theta_0) @ get_rotation_y_transform(theta_1) @ get_translation_x_transform(l_0)
     pos_joint_1 = transform_base_to_joint_1 @ pos_base
-    transform_joint_1_to_joint_2 = get_translation_x_transform(l_1) @ get_rotation_y_transform(theta_2)
-    pos_joint_2 = transform_joint_1_to_joint_2 @ pos_joint_1
+    transform_base_to_joint_2 = transform_base_to_joint_1 @ get_rotation_y_transform(-theta_2) @ get_translation_x_transform(l_1)
+    pos_joint_2 = transform_base_to_joint_2 @ pos_base
     ax.plot([pos_base[0][0], pos_joint_1[0][0]], [pos_base[1][0], pos_joint_1[1][0]], [pos_base[2][0], pos_joint_1[2][0]])
     ax.plot([pos_joint_1[0][0], pos_joint_2[0][0]], [pos_joint_1[1][0], pos_joint_2[1][0]], [pos_joint_1[2][0], pos_joint_2[2][0]])
+
+    # Check that arm distances are correct
+    dist_b_j1 = ((pos_base[0][0] - pos_joint_1[0][0])**2 + (pos_base[1][0] - pos_joint_1[1][0])**2 + (pos_base[2][0] - pos_joint_1[2][0])**2) ** 0.5
+    dist_j1_j2 = ((pos_joint_1[0][0] - pos_joint_2[0][0])**2 + (pos_joint_1[1][0] - pos_joint_2[1][0])**2 + (pos_joint_1[2][0] - pos_joint_2[2][0])**2) ** 0.5
+    print(f"Arm lengths in plot are {dist_b_j1}, {dist_j1_j2}")
+
     plt.show()
-
-
-draw_snapshot(1, 1, pi/4, pi/4, pi/4)     # TODO: Remove debugging line
