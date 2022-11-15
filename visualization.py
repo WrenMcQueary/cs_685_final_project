a0c4545a07caf83ce47a310c6a2eff08a01b0a32
Wren McQueary <wren.mcqueary@gmail.com>
2022-11-14 23:53:38 -0500

    Removed an old breakpoint

--- visualization.py
+++ visualization.py
@@ -29,8 +29,7 @@ def update_lines(frameno: int, lines: list, l_0: float, l_1: float, theta_0_sequ
     pos_joint_1 = transform_base_to_joint_1 @ pos_base
     transform_base_to_joint_2 = get_forward_transform_base_to_joint_2(l_0, l_1, theta_0, theta_1, theta_2)
     pos_joint_2 = transform_base_to_joint_2 @ pos_base
-    #breakpoint()    # TODO: Remove debugging line
-    print(f"frameno is {frameno}\tangles are {theta_0_sequence[frameno]}\t\t{theta_1_sequence[frameno]}\t\t{theta_2_sequence[frameno]}\t\tend effector is at {pos_joint_2.T[0][:-1]}")
+    #print(f"frameno is {frameno}\tangles are {theta_0_sequence[frameno]}\t\t{theta_1_sequence[frameno]}\t\t{theta_2_sequence[frameno]}\t\tend effector is at {pos_joint_2.T[0][:-1]}")
 
     # Update the line objects
     lines[0].set_data_3d([pos_base[0][0], pos_joint_1[0][0]], [pos_base[1][0], pos_joint_1[1][0]], [pos_base[2][0], pos_joint_1[2][0]])
