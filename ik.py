60c31604349f9d44c31525dfaa50b750e15ca5ce
Wren McQueary <wren.mcqueary@gmail.com>
2022-11-14 23:52:01 -0500

    Fixed a unit error which was causing the end effector to jump around

--- ik.py
+++ ik.py
@@ -20,7 +20,7 @@ def ccd(goal: np.array, l_0: float, l_1: float, angles: list, iterations=5) -> [
             # Find the value for this angle that minimizes the distance between the end effector and the goal position
             best_possible_angle = 0
             best_possible_distance = float("inf")
-            for possible_angle in np.arange(0, 360, 1):
+            for possible_angle in np.arange(0, 2*pi, 2*pi/360):
                 angle_list_under_consideration = angles
                 angle_list_under_consideration[aa] = possible_angle
                 pos_end_effector = get_forward_transform_base_to_joint_2(l_0, l_1, angle_list_under_consideration[0], angle_list_under_consideration[1], angle_list_under_consideration[2]) @ pos_base
